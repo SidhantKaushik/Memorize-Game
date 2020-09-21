ceb294004b6ff5ec65d14588f0e0f61a8ac69f28
Sidhant-Kaushik <sidhant9101@gmail.com>
2020-09-20 23:10:10 -0600

    MemorizeGame

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,42 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Sidhant Kaushik on 2020-07-09.
+//  Copyright © 2020 Sidhant Kaushik. All rights reserved.
+//
+
+import SwiftUI
+struct randomNumber {
+    static var random = Int.random(in: 2...5)
+}
+class EmojiMemoryGame{
+    /*private(set)/*glass door meaning only can view it but cant change it*/ var game: MemoryGame<String>*/
+    private var game: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻","🎃","🕷","🤮"]
+        
+        return MemoryGame<String>(numberOfPairsOfCards: randomNumber.random) {pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+    // MARK - Access to the Model
+    var cards: Array<MemoryGame<String>.Card>{
+        game.cards
+    }
+    
+    // MARK: -Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card){
+        game.choose(card: card)
+    }
+    
+}
+
+struct EmojiMemoryGame_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,90 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by Sidhant Kaushik on 2020-07-08.
+//  Copyright © 2020 Sidhant Kaushik. All rights reserved.
+//
+
+import SwiftUI
+struct EmojiMemoryGameView: View {
+    var viewModel: EmojiMemoryGame
+    var body: some View {
+        return HStack/*spacing: 2, could include this to remove the space between the rows*/{
+            return ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture{ self.viewModel.choose(card: card)}
+                
+            }
+    }
+        .foregroundColor(Color.orange) //tell every view inside of the z stack to have a foreground color of orange
+        .frame(width: 300, height: 500)
+            .padding()
+        .aspectRatio(2/3, contentMode: .fit)
+        // could put .font(Font.largeTitle) here if wanted
+}
+}
+
+
+struct CardView: View{
+    var card: MemoryGame<String>.Card
+    
+    
+    var body: some View{
+        ZStack/*content: not nessasary*/{
+            if card.isFaceUp{
+                RoundedRectangle(cornerRadius: 10.0)
+                   .stroke(lineWidth: 3)//.foregroundColor(Color.blue) //overriding the zstack color
+               RoundedRectangle(cornerRadius: 10.0)
+                   .fill(Color.white)
+               
+                if randomNumber.random > 3 {
+                Text(card.content)
+                    .font(.body)
+                }
+                else {
+                    Text(card.content)
+                        .font(.largeTitle)
+                }
+            }
+                          }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct EmojiMemoryGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    }
+}
